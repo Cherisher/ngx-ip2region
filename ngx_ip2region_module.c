a6120c95af5b203927758d61a9744c0d84195707
yuhuadong <yuhuadong@cmiot.chinamobile.com>
2019-08-14 10:55:26 +0800

    fix bug

--- ngx_ip2region_module.c
+++ ngx_ip2region_module.c
@@ -13,6 +13,22 @@ static void ngx_ip2region_exit_process(ngx_cycle_t *cycle);
 
 static ip2region_entry g_ip2region_entry;
 
+
+search_func_ptr ip2region_search_func;
+
+
+#define NGX_IP2REGION_MEMORY 0
+#define NGX_IP2REGION_BINARY 1
+#define NGX_IP2REGION_BTREE 2
+
+static ngx_conf_enum_t  ngx_ip2region_search_algo[] = {
+    { ngx_string("memory"), NGX_IP2REGION_MEMORY },
+    { ngx_string("binary"), NGX_IP2REGION_BINARY },
+    { ngx_string("btree"), NGX_IP2REGION_BTREE},
+    { ngx_null_string, 0 }
+};
+
+
 static ngx_command_t ngx_ip2region_commands[] = {
 
     {
@@ -22,6 +38,15 @@ static ngx_command_t ngx_ip2region_commands[] = {
         0,
         offsetof(ngx_ip2region_conf_t, db_file),
         NULL
+    },
+
+    {
+        ngx_string("ip2region_algo"),
+        NGX_MAIN_CONF | NGX_DIRECT_CONF | NGX_CONF_TAKE1,
+        ngx_conf_set_enum_slot,
+        0,
+        offsetof(ngx_ip2region_conf_t, algo),
+        &ngx_ip2region_search_algo
     },
 
     ngx_null_command
@@ -60,6 +85,7 @@ ngx_ip2region_create_conf(ngx_cycle_t *cycle)
         return NULL;
     }
 
+    ip2region_conf->algo = NGX_CONF_UNSET_UINT;
     return ip2region_conf;
 }
 
@@ -69,7 +95,8 @@ ngx_ip2region_init_conf(ngx_cycle_t *cycle, void *conf)
     ngx_ip2region_conf_t  *ip2region_conf = (ngx_ip2region_conf_t *)conf;
 
     if(ip2region_conf->db_file.len == 0) {
-        ngx_str_set(&ip2region_conf->db_file, "conf/ip2region.db");
+        //ngx_str_set(&ip2region_conf->db_file, "conf/ip2region.db");
+        return NGX_CONF_OK;
     }
 
     if(ngx_conf_full_name(cycle, &ip2region_conf->db_file, 0) != NGX_OK) {
@@ -77,6 +104,11 @@ ngx_ip2region_init_conf(ngx_cycle_t *cycle, void *conf)
     }
 
     ngx_log_debug1(NGX_LOG_INFO, ngx_cycle->log, 0, "%V", &ip2region_conf->db_file);
+
+    if(ip2region_conf->algo == NGX_CONF_UNSET_UINT) {
+        ip2region_conf->algo = NGX_IP2REGION_MEMORY;
+    }
+
     return NGX_CONF_OK;
 }
 
@@ -85,22 +117,32 @@ ngx_ip2region_init_process(ngx_cycle_t *cycle)
 {
     ngx_ip2region_conf_t *conf = (ngx_ip2region_conf_t *)ngx_get_conf(ngx_cycle->conf_ctx, ngx_ip2region_module);
 
+    if(conf->db_file.len == 0) {
+        ngx_log_debug0(NGX_LOG_INFO, ngx_cycle->log, 0, "ip2region not init");
+        return NGX_OK;
+    }
+
     if(!ip2region_create(&g_ip2region_entry, (char *)conf->db_file.data)) {
         ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "ip2region create failed: %V", &conf->db_file);
         return NGX_ERROR;
     }
 
-    ngx_log_debug0(NGX_LOG_INFO, ngx_cycle->log, 0, "ip2region create succcess");
-    search_func_ptr = ip2region_binary_search;
-#if 0
-    ngx_str_t addr_text, isp = ngx_null_string, city = ngx_null_string;
-    ngx_str_set(&addr_text, "183.230.102.103");
+    switch(conf->algo) {
+        default:
+        case NGX_IP2REGION_MEMORY:
+            ip2region_search_func = ip2region_memory_search;
+            break;
 
-    if(NGX_OK == ngx_ip2region_search(&addr_text, &isp, &city)) {
-        ngx_log_error(NGX_LOG_INFO, ngx_cycle->log, 0, "ip2region: %V %V", &isp, &city);
+        case NGX_IP2REGION_BINARY:
+            ip2region_search_func = ip2region_binary_search;
+            break;
+
+        case NGX_IP2REGION_BTREE:
+            ip2region_search_func = ip2region_btree_search;
+            break;
     }
 
-#endif
+    ngx_log_error(NGX_LOG_INFO, ngx_cycle->log, 0, "ip2region create succcess: algo=%ui", conf->algo);
     return NGX_OK;
 }
 
@@ -179,45 +221,41 @@ ngx_ip2region_search(ngx_str_t *addr_text, ngx_str_t *isp, ngx_str_t *city)
         return NGX_ERROR;
     }
 
-    if(!search_func_ptr) {
-        ngx_log_debug(NGX_LOG_DEBUG, ngx_cycle->log, 0, "set method ip2region_memory_search");
-        search_func_ptr = ip2region_memory_search;
-    }
-
     datablock_entry datablock;
     ngx_memzero(&datablock, sizeof(datablock));
-    uint_t rc = search_func_ptr(&g_ip2region_entry, (uint_t)addr_binary, &datablock);
+    uint_t rc = ip2region_search_func(&g_ip2region_entry, (uint_t)addr_binary, &datablock);
 
     if(!rc) {
-        ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "search failed");
+        ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "%V search failed", addr_text);
         return NGX_ERROR;
     }
 
-    ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "%s", datablock.region);
-#if 0
+    ngx_log_debug2(NGX_LOG_INFO, ngx_cycle->log, 0, "%V: %s", addr_text, datablock.region);
     //城市Id 国家|区域|省份|城市|ISP
     u_char *end = (u_char *)(datablock.region + ngx_strlen(datablock.region));
     u_char *pos = ngx_strrchr((u_char *)datablock.region, end, '|');
 
     if(!pos) {
-        ngx_log_error(NGX_LOG_WARN, ngx_cycle->log, 0, "search isp failed");
+        ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "search isp failed");
         return NGX_ERROR;
     }
 
     u_char isp_buffer[64] = {0};
     ngx_snprintf(isp_buffer, sizeof(isp_buffer), "%*s%Z", end - pos, pos + 1);
-    end = pos - 1;
-    u_char *pos_city = ngx_strrchr((u_char *)datablock.region, end, '|');
+    ngx_log_debug3(NGX_LOG_INFO, ngx_cycle->log, 0, "%V: %s [%s]", addr_text, datablock.region, (char *)isp_buffer);
 
-    if(!pos_city) {
-        ngx_log_error(NGX_LOG_WARN, ngx_cycle->log, 0, "search city failed");
-        return NGX_ERROR;
+    if(city) {
+        end = pos - 1;
+        u_char *pos_city = ngx_strrchr((u_char *)datablock.region, end, '|');
+
+        if(!pos_city) {
+            ngx_log_debug0(NGX_LOG_ERR, ngx_cycle->log, 0, "search city failed");
+            return NGX_ERROR;
+        }
+
+        u_char city_buffer[64] = {0};
+        ngx_snprintf(city_buffer, sizeof(city_buffer), "%*s%Z", end - pos_city, pos_city + 1);
     }
 
-    u_char city_buffer[64] = {0};
-    ngx_snprintf(city_buffer, sizeof(city_buffer), "%*s%Z", end - pos_city, pos_city + 1);
-
-    ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "[%s(%V)] [%s]", isp_buffer, isp, city_buffer);
-#endif
     return NGX_OK;
 }
